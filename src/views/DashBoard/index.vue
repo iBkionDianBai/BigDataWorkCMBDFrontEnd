6d67080e075ff8ba97708d13829a10c804c4c434
iBkionDianBai <3106701243@qq.com>
2025-10-17 23:46:54 +0800

    dashboard commit

--- src/views/DashBoard/index.vue
+++ src/views/DashBoard/index.vue
@@ -3,7 +3,7 @@
 
 
     <iframe
-        src="http://apps.ce.bktencent.com/bk-vision/embed/?uid=MsXAnAXg2ALowhtXWo9C7a&bk_app_id=&bk_app_list=['bkcourse-xaut']&name=&show_copyright=True&watermark=True&time_readonly=False&show_time=True&show_refresh=True&start_time=now/d&end_time=now/d&preview=False&hide_toolbox=False&hide_filter=False&panels=&refresh=False"
+        src="https://apps.ce.bktencent.com/bk-vision/embed/?uid=MsXAnAXg2ALowhtXWo9C7a&bk_app_id=&bk_app_list=['bkcourse-xaut']&name=&show_copyright=True&watermark=True&time_readonly=False&show_time=True&show_refresh=True&start_time=now/d&end_time=now/d&preview=False&hide_toolbox=False&hide_filter=False&panels=&refresh=False"
         style="height: 100%;width: 100%;border: 1px solid rgb(220, 222, 229);">
     </iframe>
 
